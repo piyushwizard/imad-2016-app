07fc2c3206ec1f6a6f3cbc4f9435f0c5c69c355a
piyushwizard <ce16m107@smail.iitm.ac.in>
2016-10-10 14:31:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -102,8 +102,8 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get('/ui/madi.jpeg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.jped'));
 });
 
 
