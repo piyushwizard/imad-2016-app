7f8775c6acafa95347cec33b6bdc910dce9a3a30
piyushwizard <ce16m107@smail.iitm.ac.in>
2016-10-10 09:35:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,13 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'New value';
 
 // move image
-var img = document.getElementById('madi');
+var img = document.getElementById('madi
+var marginLeft = 0;
+function moveRight(){
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
 img.onclick = function (){
-    img.style.marginLeft = '120px';
+    var interval = setInterval(moveRight, 100);
+    
 };
